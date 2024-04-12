64b8537564d38bebfa413df6644f212d31e02dd4
gumrahsindar <gsindar@gmail.com>
2024-04-12 15:24:19 +0300

    complete main style definitions

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      rubik: ['Rubik', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        purple: '#A729F5',
+        'dark-blue': '#313E51',
+        'dark-blue-grey': '#3B4D66',
+        'greyish-blue': '##626C7F',
+        'light-blue': '#ABC1E1',
+        'light-grey': '#F4F6FA',
+        green: '#26D782',
+        red: '#EE5454',
+      },
+      backgroundImage: {
+        'mobile-dark': "url('/images/pattern-background-mobile-dark.svg')",
+        'mobile-light': "url('/images/pattern-background-mobile-light.svg')",
+        'desktop-dark': "url('/images/pattern-background-desktop-dark.svg')",
+        'desktop-light': "url('/images/pattern-background-desktop-light.svg')",
+      },
+    },
   },
   plugins: [],
 }
